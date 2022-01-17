9f73a9978d93ed90c12a2070bf5b70cb66b2b24d
jacobnathanson <jacobnath93@gmail.com>
2022-01-16 19:09:41 -0500

    basic slider

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import './App.css';
 import React, { useState } from 'react';
- import { useSelector, useDispatch, } from 'react-redux'
+import { useSelector, useDispatch, } from 'react-redux'
 // import { ADD_TODO, TOGGLE_COMPLETE, CLEAR_TASKS, DELETE_TASKS, DELETE_SINGLE_TASK } from './redux/todoSlice'
 // clone
 function App() {
   // const todos = useSelector((state) => state.todoReducer.todoList);
   // const [todoInput, setTodoInput] = useState('');
+  const [guestNum, setGuestNum] = useState(50)
   const dispatch = useDispatch();
 
 
   return (
     <div>
       <h1>price estimator</h1>
+      <h2>How many guests will you have?</h2>
+      <h3>Current guest count: {guestNum}</h3>
+      <label htmlFor="guests"> </label>
+      <input
+        id="guests"
+        type="range"
+        min="50"
+        max="500"
+        value={guestNum}
+        onChange={(e) => setGuestNum(e.target.value)}
+      />
 
-      <h2>guests</h2>
 
       <h2>addons</h2>
       <div>
