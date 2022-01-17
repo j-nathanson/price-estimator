936ca2b1512aa49944e99b7d326e06a8f1a01ab0
jacobnathanson <jacobnath93@gmail.com>
2022-01-16 20:00:17 -0500

    side cost action

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@ import './App.css';
 import React, { useState } from 'react';
 import { useSelector, useDispatch, } from 'react-redux'
 
-import { changeGuestNum } from './redux/estimatorSlice'
+import { changeGuestNum, toggleSides } from './redux/estimatorSlice'
 
 function App() {
   const costPerGuest = useSelector(state => state.estimatorReducer.costs.costPerGuest);
   const guestNum = useSelector(state => state.estimatorReducer.guestNum);
+  const isSideChecked = useSelector(state => state.estimatorReducer.isSideChecked);
+  const sideCost = useSelector(state => state.estimatorReducer.costs.sideCost);
   const dispatch = useDispatch();
 
+  const total = () => {
+    let cost = guestNum * costPerGuest;
+    if (isSideChecked) {
+      cost += sideCost
+    }
+    return cost
+  }
 
   return (
     <div>
@@ -32,12 +41,15 @@ function App() {
 
       <h2>addons</h2>
       <div>
-        <button>Sides</button>
-        <button>Entrees</button>
-        <button>Desserts</button>
+        <label htmlFor="sides">sides</label>
+        <input type="checkbox" id="sides" onChange={() => dispatch(toggleSides())} />
+        <label htmlFor="entrees">entrees</label>
+        <input type="checkbox" id="sides" />
+        <label htmlFor="desserts">desserts</label>
+        <input type="checkbox" id="sides" />
       </div>
 
-      <h2>Final cost: ${guestNum * costPerGuest}</h2>
+      <h2>Final cost: ${total()}</h2>
     </div>
   );
 }
--- src/redux/estimatorSlice.js
+++ src/redux/estimatorSlice.js
@@ -1,9 +1,11 @@
 import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
-    guestNum:50,
+    guestNum: 50,
+    isSideChecked: false,
     costs: {
-        costPerGuest: 14
+        costPerGuest: 14,
+        sideCost: 100
     }
 }
 
@@ -11,14 +13,17 @@ export const estimatorSlice = createSlice({
     name: 'estimator',
     initialState,
     reducers: {
-        changeGuestNum: (state, action) =>{
+        changeGuestNum: (state, action) => {
             state.guestNum = action.payload
+        },
+        toggleSides: state => {
+            state.isSideChecked = !state.isSideChecked
         }
-       
+
     },
 })
 
 // Action creators are generated for each case reducer function
- export const { changeGuestNum } = estimatorSlice.actions
+export const { changeGuestNum, toggleSides } = estimatorSlice.actions
 
 export default estimatorSlice.reducer
